f2d67961260dc1be8a6f2a5df33e3857b224f826
Cory Burns <corypburns@gmail.com>
2025-06-02 10:00:37 -0400

    -m

--- SQLiCSIC.py
+++ SQLiCSIC.py
@@ -1,8 +1,10 @@
-import pandas as pd, time, csv, os
+import pandas as pd, time, csv, os, matplotlib.pyplot as mpl
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from array import array
 
 # CSV Headers
 file_name = 'output.csv'
@@ -11,8 +13,8 @@ joined_file_path = os.path.join(file_path, file_name)
 output_path = '/home/cory/code/CISResearchSummer2025/Outputs/NaiveBayes'
 joined_output_path = os.path.join(output_path, 'Output.csv')
 
-if not os.path.exists(joined_file_path):
-    with open('output.csv', mode='w') as f:
+if not os.path.exists(joined_output_path):
+    with open(joined_output_path, mode='w') as f:
         f.write("Classification Actual Method Content URL")
 
 
@@ -46,6 +48,11 @@ model.fit(X_train_vec, y_train)
 # Predict and evaluate
 X_test_index = X_test.index
 y_pred = model.predict(X_test_vec)
+
+# True Variables Matrix
+tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
+
+# Printing
 try:
     with open(joined_output_path, mode='w') as f:
         for i, prediction in zip(X_test_index, y_pred):
@@ -57,8 +64,17 @@ try:
             print(print_statement)
             f.write(f"{label} {prediction} {method} {content} {url}\n")
             time.sleep(.5)
+    print("True Positives (TP): {tp}\nTrue Negatives (TN): {tn}\nFalse Positives (FP): {fp}\nFalse Negatives (FN): {fn}")
 except KeyboardInterrupt:
     print(classification_report(y_test, y_pred))
+    print(f"True Positives (TP): {tp}\nTrue Negatives (TN): {tn}\nFalse Positives (FP): {fp}\nFalse Negatives (FN): {fn}")
+    ConfusionMatrixDisplay.from_predictions(y_test, y_pred)
+    mpl.show()
     os.chdir(output_path)
     with open('results.txt', mode='w') as f:
         f.write(classification_report(y_test, y_pred))
+        f.write("\n=== Confusion Matrix Stats ===\n")
+        f.write(f"True Positives (TP): {tp}\n")
+        f.write(f"True Negatives (TN): {tn}\n")
+        f.write(f"False Positives (FP): {fp}\n")
+        f.write(f"False Negatives (FN): {fn}\n")
